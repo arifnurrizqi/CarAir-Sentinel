3c2acd8fa18ad391d05e518172a58b5b3b7a6060
arifnurrizqi <arifnurrizqi@gmail.com>
2024-06-04 19:43:38 +0700

    add decission for rgb module

--- CarAir-Sentinel-arduino/CarAir-Sentinel-arduino.ino
+++ CarAir-Sentinel-arduino/CarAir-Sentinel-arduino.ino
@@ -20,16 +20,16 @@
 
 //Declare Sensor
 MQUnifiedsensor MQ135(CHIP, Voltage_Resolution, ADC_Bit_Resolution, SENSOR_PIN, TYPE);
-BUZZER_PIN 
-const char* ssid     = "ARNUR";
-const char* password = "tanyamama";
+
+const char* ssid = "ARNUR";
+const char* password = "takonmama";
 
 void setup() {
-  //Init the serial port communication - to debug the library
-  Serial.begin(115200); //Init serial port
+  // Init the serial port communication - to debug the library
+  Serial.begin(115200); // Init serial port
   pinMode(BUZZER_PIN, OUTPUT);
   pinMode(RED_PIN, OUTPUT);
-  pinMode(GREEN_PIN_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
   pinMode(BLUE_PIN, OUTPUT);
   pinMode(RELAY_PIN, OUTPUT);
 
@@ -50,39 +50,33 @@ void setup() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 
-  //Set math model to calculate the PPM concentration and the value of constants
-  MQ135.setRegressionMethod(1); //_PPM =  a*ratio^b
+  // Set math model to calculate the PPM concentration and the value of constants
+  MQ135.setRegressionMethod(1); // _PPM =  a*ratio^b
   
   /*****************************  MQ Init ********************************************/ 
-  //Remarks: Configure the pin of arduino as input.
-  /************************************************************************************/ 
   MQ135.init(); 
-  /* 
-    //If the RL value is different from 10K please assign your RL value with the following method:
-    MQ135.setRL(10);
-  */
-  /*****************************  MQ CAlibration ********************************************/ 
-  // Explanation: 
-  // In this routine the sensor will measure the resistance of the sensor supposedly before being pre-heated
-  // and on clean air (Calibration conditions), setting up R0 value.
-  // We recomend executing this routine only on setup in laboratory conditions.
-  // This routine does not need to be executed on each restart, you can load your R0 value from eeprom.
-  // Acknowledgements: https://jayconsystems.com/blog/understanding-a-gas-sensor
+
+  /*****************************  MQ Calibration ********************************************/ 
   Serial.print("Calibrating please wait.");
   float calcR0 = 0;
-  for(int i = 1; i<=10; i ++)
-  {
+  for(int i = 1; i<=10; i++) {
     MQ135.update(); // Update data, the arduino will read the voltage from the analog pin
     calcR0 += MQ135.calibrate(RatioMQ135CleanAir);
     Serial.print(".");
   }
   MQ135.setR0(calcR0/10);
-  Serial.println("  done!.");
+  Serial.println("  done!");
   
-  if(isinf(calcR0)) {Serial.println("Warning: Conection issue, R0 is infinite (Open circuit detected) please check your wiring and supply"); while(1);}
-  if(calcR0 == 0){Serial.println("Warning: Conection issue found, R0 is zero (Analog pin shorts to ground) please check your wiring and supply"); while(1);}
-  /*****************************  MQ CAlibration ********************************************/ 
-  Serial.println("** Values from MQ-135 ****");
+  if(isinf(calcR0)) {
+    Serial.println("Warning: Connection issue, R0 is infinite (Open circuit detected) please check your wiring and supply");
+    while(1);
+  }
+  if(calcR0 == 0) {
+    Serial.println("Warning: Connection issue found, R0 is zero (Analog pin shorts to ground) please check your wiring and supply");
+    while(1);
+  }
+
+  Serial.println("** Values from MQ-135 **");
   Serial.println("|    CO    |    CO2    |");  
 }
 
@@ -95,28 +89,55 @@ void loop() {
   MQ135.setA(110.47); MQ135.setB(-2.862); // Configure the equation to calculate CO2 concentration value
   float CO2 = MQ135.readSensor(); // Sensor will read PPM concentration using the model, a and b values set previously or from the setup
 
+  CO2 += 400; // Add 400 to the CO2 value to account for baseline atmospheric CO2
+
   Serial.print("|   "); Serial.print(CO); 
-  // Note: 400 Offset for CO2 source: https://github.com/miguel5612/MQSensorsLib/issues/29
-  /*
-  Motivation:
-  We have added 400 PPM because when the library is calibrated it assumes the current state of the
-  air as 0 PPM, and it is considered today that the CO2 present in the atmosphere is around 400 PPM.
-  https://www.lavanguardia.com/natural/20190514/462242832581/concentracion-dioxido-cabono-co2-atmosfera-bate-record-historia-humanidad.html
-  */
-  Serial.print("   |   "); Serial.print(CO2 + 400); 
+  Serial.print("   |   "); Serial.print(CO2); 
   Serial.println("   |"); 
-  /*
-    Exponential regression:
-  GAS      | a      | b
-  CO       | 605.18 | -3.937  
-  CO2      | 110.47 | -2.862
-  */
 
-  delay(500); //Sampling frequency
+  if ((CO < 50) || (CO2 < 1000)) {
+    // Kadar CO < 50 atau CO2 < 1000
+    digitalWrite(GREEN_PIN, HIGH);
+    digitalWrite(RED_PIN, LOW);
+    digitalWrite(BLUE_PIN, LOW);
+    digitalWrite(BUZZER_PIN, LOW);
+    Serial.println("Status: Aman - LED Hijau Menyala");
+  } 
+  else if ((CO >= 51 && CO <= 200) || (CO2 >= 1001 && CO2 <= 2000)) {
+    // Kadar CO > 51 < 200 atau CO2 > 1001 < 2000
+    digitalWrite(GREEN_PIN, LOW);
+    digitalWrite(RED_PIN, LOW);
+    digitalWrite(BLUE_PIN, LOW);
+    digitalWrite(RED_PIN, HIGH);
+    digitalWrite(GREEN_PIN, HIGH);
+    tone(BUZZER_PIN, 1000); // Buzzer on
+    Serial.println("Status: Waspada - LED Kuning Menyala, Buzzer Aktif");
+    delay(5000);
+    noTone(BUZZER_PIN); // Buzzer off
+  } 
+  else if ((CO >= 201 && CO <= 400) || (CO2 >= 2001 && CO2 <= 2500)) {
+    // Kadar CO > 201 < 400 atau CO2 > 2001 < 2500
+    digitalWrite(GREEN_PIN, LOW);
+    digitalWrite(BLUE_PIN, LOW);
+    digitalWrite(RED_PIN, HIGH);
+    tone(BUZZER_PIN, 1000); // Buzzer on
+    Serial.println("Status: Bahaya - LED Merah Menyala, Buzzer Aktif");
+    delay(2500);
+    noTone(BUZZER_PIN); // Buzzer off
+  } 
+  else if (CO > 400 || CO2 > 2500) {
+    // Kadar CO > 400 atau CO2 > 2500
+    digitalWrite(GREEN_PIN, LOW);
+    digitalWrite(RED_PIN, HIGH);
+    digitalWrite(BLUE_PIN, HIGH);
+    tone(BUZZER_PIN, 1000); // Buzzer on
+    Serial.println("Status: Sangat Berbahaya - LED Ungu Menyala, Buzzer Aktif, Relay Aktif");
+    delay(1000);
+    noTone(BUZZER_PIN); // Buzzer off
+    digitalWrite(RELAY_PIN, HIGH); // Relay on
+    delay(10000); // Relay on for 10 seconds
+    digitalWrite(RELAY_PIN, LOW); // Relay off
+  }
 
-  // Decission
-  // Kadar CO < 50 atau CO2 < 1000, LED RGB menyala warna hijau;
-  // Kadar CO > 51 < 200 atau CO2 > 1001 < 2000, LED RGB menyala Berwarna Kuning, Buzzer menyala dgn delay 5000;
-  // Kadar CO > 201 < 400 atau CO2 > 2001 < 2500, LED RGB menyala Berwarna Merah, Buzzer menyala dgn delay 2500;
-  // Kadar CO > 400 atau CO2 > 2500, LED RGB menyala Berwarna Ungu, Buzzer menyala dgn delay 1000, Notif Telegram dan menyalakan relay dengan durasi tertentu;
+  delay(500); 
 }
