2e15f723f14ee7805e3724de4136938a7841a9ae
Arif Nur Rizqi <arifnurrizqi@gmail.com>
2024-06-08 21:03:27 +0700

    add blynk program

--- CarAir-Sentinel-arduino/CarAir-Sentinel-arduino.ino
+++ CarAir-Sentinel-arduino/CarAir-Sentinel-arduino.ino
@@ -1,14 +1,20 @@
-// Include the library
-#include <WiFi.h>
-#include <MQUnifiedsensor.h>
+// Blynk Configuration
+#define BLYNK_TEMPLATE_ID           "TMPL6N6ZN0S9E"
+#define BLYNK_TEMPLATE_NAME         "CAR AIR SENTINEL"
+#define BLYNK_AUTH_TOKEN            "XibW_d_zhJunuX90XhGidobavWs27mIP"
+
+// Include the libraries
+#include <WiFi.h>                  // Library for WiFi ESP32
+#include <WiFiClient.h>            // Library WiFi Client ESP32
+#include <MQUnifiedsensor.h>       // Library for sensor MQ Series 
+#include <BlynkSimpleEsp32.h>      // Library for blynk IoT
 
 // MQ-135 Configuration
-#define CHIP "ESP-32" // Wemos ESP-32 or other board, whatever have ESP32 
-#define Voltage_Resolution 3.3 // 3V3 <- IMPORTANT. Source: https://randomnerdtutorials.com/esp32-adc-analog-read-arduino-ide/
-#define TYPE "MQ-135" //MQ135
-#define ADC_Bit_Resolution 12 // For arduino ESP32
-#define RatioMQ135CleanAir 3.6 //RS / R0 = 3.6 ppm  
-//#define calibration_button 13 //Pin to calibrate your sensor
+#define CHIP                  "ESP-32" // Wemos ESP-32 or other board, whatever have ESP32 
+#define TYPE                  "MQ-135" // MQ135
+#define Voltage_Resolution    3.3 // 3V3 <- IMPORTANT. Source: https://randomnerdtutorials.com/esp32-adc-analog-read-arduino-ide/
+#define ADC_Bit_Resolution    12 // For arduino ESP32
+#define RatioMQ135CleanAir    3.6 // RS / R0 = 3.6 ppm  
 
 // Pinout Configuration
 #define SENSOR_PIN 34 // Analog input ESP32
@@ -18,15 +24,32 @@
 #define BLUE_PIN 19   // Pin LED BLUE RGB Module
 #define RELAY_PIN 5   // Pin For Relay Module
 
-//Declare Sensor
+// Declare Sensor
 MQUnifiedsensor MQ135(CHIP, Voltage_Resolution, ADC_Bit_Resolution, SENSOR_PIN, TYPE);
 
 const char* ssid = "ARNUR";
 const char* password = "takonmama";
 
+// Blynk setup
+BlynkTimer timer;
+
+void sendToBlynk(float CO, float CO2, String status, int relayStatus) {
+  Blynk.virtualWrite(V0, CO2);          // Send CO2 data to Blynk
+  Blynk.virtualWrite(V1, CO);           // Send CO data to Blynk
+  Blynk.virtualWrite(V2, status);       // Send status to Blynk
+  Blynk.virtualWrite(V3, relayStatus);  // Send relay status to Blynk
+}
+
+// Timer variables
+unsigned long previousMillis = 0;
+unsigned long previousMillisRelay = 0;
+bool relayOn = false;   // Relay Status
+
 void setup() {
   // Init the serial port communication - to debug the library
   Serial.begin(115200); // Init serial port
+
+  // Setting pinMode ESP32
   pinMode(BUZZER_PIN, OUTPUT);
   pinMode(RED_PIN, OUTPUT);
   pinMode(GREEN_PIN, OUTPUT);
@@ -41,8 +64,8 @@ void setup() {
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
+    delay(500);
+    Serial.print(".");
   }
 
   Serial.println("");
@@ -59,83 +82,121 @@ void setup() {
   /*****************************  MQ Calibration ********************************************/ 
   Serial.print("Calibrating please wait.");
   float calcR0 = 0;
-  for(int i = 1; i<=10; i++) {
+  for (int i = 1; i <= 10; i++) {
     MQ135.update(); // Update data, the arduino will read the voltage from the analog pin
     calcR0 += MQ135.calibrate(RatioMQ135CleanAir);
     Serial.print(".");
   }
-  MQ135.setR0(calcR0/10);
+  MQ135.setR0(calcR0 / 10);
   Serial.println("  done!");
   
-  if(isinf(calcR0)) {
+  if (isinf(calcR0)) {
     Serial.println("Warning: Connection issue, R0 is infinite (Open circuit detected) please check your wiring and supply");
-    while(1);
+    while (1);
   }
-  if(calcR0 == 0) {
+  if (calcR0 == 0) {
     Serial.println("Warning: Connection issue found, R0 is zero (Analog pin shorts to ground) please check your wiring and supply");
-    while(1);
+    while (1);
   }
 
   Serial.println("** Values from MQ-135 **");
   Serial.println("|    CO    |    CO2    |");  
+
+  // Initialize Blynk
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, password);
+
+  // Setup a function to be called every second
+  timer.setInterval(1000L, []() {
+    MQ135.update(); // Update data, the arduino will read the voltage from the analog pin
+
+    MQ135.setA(605.18); MQ135.setB(-3.937); // Configure the equation to calculate CO concentration value
+    float CO = MQ135.readSensor(); // Sensor will read PPM concentration using the model, a and b values set previously or from the setup
+
+    MQ135.setA(110.47); MQ135.setB(-2.862); // Configure the equation to calculate CO2 concentration value
+    float CO2 = MQ135.readSensor(); // Sensor will read PPM concentration using the model, a and b values set previously or from the setup
+
+    CO2 += 400; // Add 400 to the CO2 value to account for baseline atmospheric CO2
+
+    Serial.print("|   "); Serial.print(CO);
+    Serial.print("   |   "); Serial.print(CO2);
+    Serial.println("   |");
+
+    String status = "";
+    int relayStatus = 0;
+
+    unsigned long currentMillis = millis();
+
+    if ((CO < 50) || (CO2 < 1000)) {
+      // Kadar CO < 50 atau CO2 < 1000
+      digitalWrite(GREEN_PIN, LOW);
+      digitalWrite(RED_PIN, HIGH);
+      digitalWrite(BLUE_PIN, HIGH);
+      digitalWrite(BUZZER_PIN, LOW);
+      status = "Aman";
+      Serial.println("Status: Aman - LED Hijau Menyala");
+
+      Blynk.logEvent("gas_safe_alarm", "AMAN! Kadar CO atau CO2 RENDAH");
+    }
+    else if ((CO >= 51 && CO <= 200) || (CO2 >= 1001 && CO2 <= 2000)) {
+      // Kadar CO > 51 < 200 atau CO2 > 1001 < 2000
+      if (currentMillis - previousMillis >= 5000) {
+        previousMillis = currentMillis;
+        digitalWrite(RED_PIN, LOW);
+        digitalWrite(BLUE_PIN, HIGH);
+        digitalWrite(GREEN_PIN, LOW);
+        digitalWrite(BUZZER_PIN, HIGH); // Buzzer on 
+        status = "Waspada";
+        Serial.println("Status: Waspada - LED Kuning Menyala, Buzzer Aktif");
+
+        Blynk.logEvent("gas_alarm", "WASPADA! Kadar CO atau CO2 cukup tinggi");
+      }
+    }
+    else if ((CO >= 201 && CO <= 400) || (CO2 >= 2001 && CO2 <= 2500)) {
+      // Kadar CO > 201 < 400 atau CO2 > 2001 < 2500
+      if (currentMillis - previousMillis >= 2500) {
+        previousMillis = currentMillis;
+        digitalWrite(GREEN_PIN, HIGH);
+        digitalWrite(BLUE_PIN, HIGH);
+        digitalWrite(RED_PIN, LOW);
+        digitalWrite(BUZZER_PIN, HIGH); // Buzzer on 
+        status = "Bahaya";
+        Serial.println("Status: Bahaya - LED Merah Menyala, Buzzer Aktif");
+
+        Blynk.logEvent("gas_alarm", "BAHAYA! Kadar CO atau CO2 tinggi");
+      }
+    }
+    else if (CO > 400 || CO2 > 2500) {
+      // Kadar CO > 400 atau CO2 > 2500
+      if (currentMillis - previousMillis >= 1000) {
+        previousMillis = currentMillis;
+        digitalWrite(GREEN_PIN, HIGH);
+        digitalWrite(RED_PIN, LOW);
+        digitalWrite(BLUE_PIN, LOW);
+        digitalWrite(BUZZER_PIN, HIGH); // Buzzer on 
+        status = "Sangat Berbahaya";
+        Serial.println("Status: Sangat Berbahaya - LED Ungu Menyala, Buzzer Aktif, Relay Aktif");
+        digitalWrite(RELAY_PIN, HIGH); // Relay on
+        relayOn = true;
+        relayStatus = 1; // data for blynk
+        previousMillisRelay = currentMillis;
+
+        Blynk.logEvent("gas_alarm", "SANGAT BERBAHAYA! Kadar CO atau CO2 sangat tinggi");
+      }
+    }
+
+    // Turn off relay after 10 seconds
+    if (relayOn && (currentMillis - previousMillisRelay >= 10000)) {
+      digitalWrite(RELAY_PIN, LOW); // Relay off
+      relayOn = false;
+      relayStatus = 0; // data for blynk
+    }
+
+    // Send data to Blynk
+    sendToBlynk(CO, CO2, status, relayStatus);
+  });
 }
 
 void loop() {
-  MQ135.update(); // Update data, the arduino will read the voltage from the analog pin
-
-  MQ135.setA(605.18); MQ135.setB(-3.937); // Configure the equation to calculate CO concentration value
-  float CO = MQ135.readSensor(); // Sensor will read PPM concentration using the model, a and b values set previously or from the setup
-
-  MQ135.setA(110.47); MQ135.setB(-2.862); // Configure the equation to calculate CO2 concentration value
-  float CO2 = MQ135.readSensor(); // Sensor will read PPM concentration using the model, a and b values set previously or from the setup
-
-  CO2 += 400; // Add 400 to the CO2 value to account for baseline atmospheric CO2
-
-  Serial.print("|   "); Serial.print(CO); 
-  Serial.print("   |   "); Serial.print(CO2); 
-  Serial.println("   |"); 
-
-  if ((CO < 50) || (CO2 < 1000)) {
-    // Kadar CO < 50 atau CO2 < 1000
-    digitalWrite(GREEN_PIN, LOW);
-    digitalWrite(RED_PIN, HIGH);
-    digitalWrite(BLUE_PIN, HIGH);
-    digitalWrite(BUZZER_PIN, LOW);
-    Serial.println("Status: Aman - LED Hijau Menyala");
-  } 
-  else if ((CO >= 51 && CO <= 200) || (CO2 >= 1001 && CO2 <= 2000)) {
-    // Kadar CO > 51 < 200 atau CO2 > 1001 < 2000
-    digitalWrite(RED_PIN, LOW);
-    digitalWrite(BLUE_PIN, HIGH);
-    digitalWrite(GREEN_PIN, LOW);
-    tone(BUZZER_PIN, 1000); // Buzzer on
-    Serial.println("Status: Waspada - LED Kuning Menyala, Buzzer Aktif");
-    delay(5000);
-    noTone(BUZZER_PIN); // Buzzer off
-  } 
-  else if ((CO >= 201 && CO <= 400) || (CO2 >= 2001 && CO2 <= 2500)) {
-    // Kadar CO > 201 < 400 atau CO2 > 2001 < 2500
-    digitalWrite(GREEN_PIN, HIGH);
-    digitalWrite(BLUE_PIN, HIGH);
-    digitalWrite(RED_PIN, LOW);
-    tone(BUZZER_PIN, 1000); // Buzzer on
-    Serial.println("Status: Bahaya - LED Merah Menyala, Buzzer Aktif");
-    delay(2500);
-    noTone(BUZZER_PIN); // Buzzer off
-  } 
-  else if (CO > 400 || CO2 > 2500) {
-    // Kadar CO > 400 atau CO2 > 2500
-    digitalWrite(GREEN_PIN, HIGH);
-    digitalWrite(RED_PIN, LOW);
-    digitalWrite(BLUE_PIN, LOW);
-    tone(BUZZER_PIN, 1000); // Buzzer on 
-    Serial.println("Status: Sangat Berbahaya - LED Ungu Menyala, Buzzer Aktif, Relay Aktif");
-    delay(1000);
-    noTone(BUZZER_PIN); // Buzzer off
-    digitalWrite(RELAY_PIN, HIGH); // Relay on
-    delay(10000); // Relay on for 10 seconds
-    digitalWrite(RELAY_PIN, LOW); // Relay off
-  }
-
-  delay(500); 
+  Blynk.run();
+  timer.run();
 }
